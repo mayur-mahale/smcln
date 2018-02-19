05b5b4481b79bdea189201b2e4230f606fb587f8
Mayur Mahale <mayur.mahale@gmail.com>
2018-02-19 21:51:57 +0530

    Sentence correction

--- doc_util.py
+++ doc_util.py
@@ -13,6 +13,15 @@ from nltk.tokenize import sent_tokenize
 def sentence_extractor(doc):
     return sent_tokenize(doc)
 
+def correction_punctuation(text):
+    import requests
+    payload = [
+        ('text', text),
+    ]
+    r = requests.post('http://bark.phon.ioc.ee/punctuator', data=payload)
+    print r.text
+
+
 #doc2 = "Clarify relationship between, in domain experts and modeler. Define role respons ibilities, desired workflow in group  Domain expert adds s g ob ject descriptions and relationships Modeler puts them into the overall model Then iteration What is the status of round trip? Drupal to xmi to EA? Yes.Is there  machine actionable feedback into Drupal? No. It is possible but some work is required. It is not clear yet if there is resources for these task. Furthermore there are different positions on the issue if the roundtrip makes sense."
 # doc2 =" This is an example sent The sentence splitter will split on sent markers Ohh really !!"
 # print sentence_correction(doc2)
---
+++ main2.py
@@ -0,0 +1,3 @@
+from doc_util import  correction_punctuation
+
+correction_punctuation("hi this is mayur i want to add punctiation however we cant")
