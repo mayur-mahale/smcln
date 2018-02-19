1f5cdc9efef1c721facd6b080688bdcbab862a75
Mayur Mahale <mayur.mahale@gmail.com>
2018-02-19 22:06:37 +0530

    sentence_extractor

--- main2.py
+++ main2.py
@@ -1,3 +1,8 @@
-from doc_util import  correction_punctuation
+from doc_util import  correction_punctuation, sentence_extractor
+file = open('transcript.txt', 'r');
+doc = file.read()
+doc = correction_punctuation(doc)
+doc_sentences = sentence_extractor(doc)
 
-correction_punctuation("hi this is mayur i want to add punctiation however we cant")
+for sentences in doc_sentences:
+    print sentences
