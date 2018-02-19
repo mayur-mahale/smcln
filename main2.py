6b7d206b1bfa12d88aaa0a08e7025e81cd175a2d
Mayur Mahale <mayur.mahale@gmail.com>
2018-02-20 00:14:39 +0530

    sentence_extractor

--- main2.py
+++ main2.py
@@ -4,5 +4,14 @@ doc = file.read()
 doc = correction_punctuation(doc)
 doc_sentences = sentence_extractor(doc)
 
-for sentences in doc_sentences:
-    print sentences
+for sentence in doc_sentences:
+    print sentence
+
+from data_preprocessor import clean_documents
+from doc_util import sentence_correction
+from topic_modeler import topic_generator
+print  "************applying corrections"
+corrected_docs = [sentence_correction(sentence) for sentence in doc_sentences]
+print corrected_docs
+clean_documents = [clean_documents(doc) for doc in corrected_docs]
+topic_generator(clean_documents)
